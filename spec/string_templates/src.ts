efe5dd657a37e7374e95c6995544f2dbe8cc7eba
Ivan Bausov <ivan.bausov@gmail.com>
2017-02-14 13:37:04 +0300

    another test for string templates

--- spec/string_templates/src.ts
+++ spec/string_templates/src.ts
@@ -1,2 +1,3 @@
 export const c1:string = `Тест01`;
-export const c1:string = `Тест01 ${test.get()} Тест02 ${test01} Тест 03`;
+export const c2:string = `Тест01 ${test.get()} Тест02 ${test01} Тест 03`;
+export const c3:string = `Test ${test}`;
